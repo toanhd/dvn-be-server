bd588b2b17e1e6cef74e4ec4b0d003b876e28ccf
ToanHD <toanhanduc@gmail.com>
2018-12-19 13:22:38 +0700

    authentication route

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
     lastName: {type: String, required: true},
     password: {type: String, required: true},
     type: {type: String, required: true},
-    email: {type: String, required: true, unique: true}
+    username: {type: String, required: true, unique: true},
+    roleID: {type: String, required: true},
 });
 
 module.exports = mongoose.model('User', userSchema);
--- routes/authentication.js
+++ routes/authentication.js
@@ -3,9 +3,72 @@ const router = express.Router();
 const sha256 = require('sha256');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const http = require('http');
+const querystring = require('querystring');
+
 
 // get all user
-router.get('/get-users', function (req, res, next) {
+router.post('/login', function (req, res, next) {
+    User.findOne({username: req.body.username},
+        function (err, user) {
+            if (err) {
+                // console.log(err);
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                })
+            }
+            if (!user) {
+                // console.log(err);
+                return res.status(404).json({
+                    title: 'No User found',
+                    error: {message: 'User not found'}
+                })
+            }
+            const token = jwt.sign({user: user}, 'dvn', {expiresIn: 7200});
+            if (sha256(req.body.password) === user.password) {
+                return res.status(200).json({
+                    login: true,
+                    userID: user._id,
+                    roleID: user.roleID,
+                    userType: user.type,
+                    token: token
+                })
+            } else {
+                res.send({
+                    login: false
+                })
+            }
+        });
+
+    // let getRqOptions = {
+    //     host: '137.116.146.224',
+    //     port: 3000,
+    //     path: '/api/Student/20138724',
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'x-api-key': 'toanhd'
+    //     }
+    // };
+    //
+    // let getStdResponse = '';
+    // http.get(getRqOptions, function (responseFromRemoteApi) {
+    //     responseFromRemoteApi.on('data', function (chunk) {
+    //         getStdResponse += chunk;
+    //     });
+    //     responseFromRemoteApi.on('end', function () {
+    //         res.status(200).json({
+    //             a: 1
+    //         })
+    //     });
+    // }).on('error', function (e) {
+    //     console.log('Error when calling remote API: ' + e.message);
+    // });
+});
+
+// get all user
+router.get('/getusers', function (req, res, next) {
     // console.log('success get all plants');
     User.find({}, function (err, result) {
         res.status(200).json({
@@ -15,8 +78,9 @@ router.get('/get-users', function (req, res, next) {
     });
 });
 
-router.post('/login', function (req, res, next) {
-    User.findOne({email: req.body.email},
+
+router.post('/loginnnnn', function (req, res, next) {
+    User.findOne({username: req.body.username},
         function (err, user) {
             if (err) {
                 // console.log(err);
@@ -40,8 +104,7 @@ router.post('/login', function (req, res, next) {
                     userType: user.type,
                     token: token
                 })
-            }
-            else {
+            } else {
                 res.send({
                     login: false
                 })
@@ -73,16 +136,15 @@ router.get('/:id', function (req, res, next) {
 
 router.post('/register', async function (req, res, next) {
     try {
-        const {firstName, lastName, password, type, email} = req.body;
+        const {firstName, lastName, password, type, username, roleID} = req.body;
         const user = new User({
-            firstName, lastName, password: sha256(password), type, email
+            firstName, lastName, password: sha256(password), type, username, roleID
         });
         res.status(201).json({
             message: 'user created',
             user: await user.save()
         })
-    }
-    catch (err) {
+    } catch (err) {
         return res.status(500).json({
             title: 'An error occurred',
             error: err
