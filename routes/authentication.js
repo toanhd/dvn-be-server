9f3e8e57e8b4f7985bc61cc18232f349054464d1
ToanHD <toanhanduc@gmail.com>
2018-12-27 00:41:54 +0700

    Update authentication.js

--- routes/authentication.js
+++ routes/authentication.js
@@ -66,10 +66,11 @@ router.post('/login', function (req, res, next) {
                     }
                     break;
                 case 'hust':
-                    if (!user.isMoE && user.roleID !== 'admin' ) {
+                    if (!user.isMoE && user.roleID !== 'admin') {
                         const token = jwt.sign({user: user}, 'dv_hust', {expiresIn: 7200});
                         if (sha256(req.body.password) === user.password) {
                             return res.status(200).json({
+                                userid: user.roleID,
                                 login: true,
                                 token: token
                             })
